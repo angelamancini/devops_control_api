808d47852c20f4bd652e0e4c31bcb48007853ddb
Angela Mancini <angela.mancini@sage.com>
2016-07-09 03:45:41 +0000

    fixed some routes

--- app/controllers/api/v1/s3_controller.rb
+++ app/controllers/api/v1/s3_controller.rb
@@ -4,18 +4,20 @@ module Api::V1
   class S3Controller < ApiController
 
     # GET /v1/s3/:bucket/file_exists
-    def file_exists
-      # http_status, data = 
+    def show
+      binding.pry
+      # http_status, data =
       S3::CopyFile.new.file_exists( params[:region],
-                                                        params[:bucket],
-                                                        params[:file]
-                                                      )
+                                    params[:bucket],
+                                    params[:file]
+                                  )
+
       # status http_status
       # data
     end
 
     # POST /v1/s3/:bucket/file_copy
-    def file_copy
+    def create
       http_status, data = S3::CopyFile.new.copy_file( params[:region],
                                                       params[:bucket],
                                                       params[:file],
@@ -25,5 +27,12 @@ module Api::V1
       data
     end
 
+    def index
+      @files = [
+        { name: 'file.txt' },
+        { name: 'image.png'}
+      ]
+      render json: @files
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
-require 'api_constraints'
-
 Rails.application.routes.draw do
-  # API definition
-  namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
-    scope module: :v1,
-          constraints: ApiConstraints.new(version: 1, default: true) do
-      get 's3/:bucket/file_exists/', to: 's3#file_exists', as: 's3_file_exists'
-      post 's3/:bucket/file_copy/', to: 's3#file_copy', as: 's3_file_copy'
-      get 'ec2/lookup_instance', to: 'ec2#lookup_instance', as: 'ec2_lookup_instance'
-      # resources :github
-      # resources :right_scale
-      # resources :cloud_flare
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints subdomain: 'api' do
+    scope module: 'api' do
+      namespace :v1 do
+        get 's3/:bucket/file_exists/', to: 's3#show', as: 's3_file_exists'
+        post 's3/:bucket/file_copy/', to: 's3#create', as: 's3_file_copy'
+        get 'ec2/lookup_instance', to: 'ec2#lookup_instance', as: 'ec2_lookup_instance'
+      end
     end
   end
 end
