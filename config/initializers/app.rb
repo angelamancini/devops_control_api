59c8b09cc51760a83bd75e3fdbef01641bdd07c9
Angela Mancini <angela.mancini@sage.com>
2016-08-12 02:51:27 +0000

    sorting out yml files

--- config/initializers/app.rb
+++
@@ -1,2 +0,0 @@
-GITHUB_CONFIG = YAML::load(ERB.new(File.read(Rails.root.join "config","github_config.yml")).result)[Rails.env]
-RIGHTSCALE_CONFIG = YAML::load(ERB.new(File.read(Rails.root.join "config","rightscale_config.yml")).result)[Rails.env]
